d072a91f49b4b642b71dd7f288e71e1e10eedc8d
adi-matraku <matrakuadi@gmail.com>
2022-01-19 17:52:38 +0100

    Commit

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+  declarations: [
+    AuthLayoutComponent,
+    MainLayoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule { }
