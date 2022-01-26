0ee70a82fe8f654df54c4cf66307faf9408c13a9
adi-matraku <matrakuadi@gmail.com>
2022-01-26 09:53:20 +0100

    Commit

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -13,19 +13,7 @@ export class MainLayoutComponent implements OnInit {
   items!: MegaMenuItem[];
   visibleSidebar1 = true;
 
-  // public showInitials = false;
-  // public initials!: string;
-  // public circleColor!: string;
-  //
-  // private colors = [
-  //   '#EB7181', // red
-  //   '#468547', // green
-  //   '#FFD558', // yellow
-  //   '#3670B2', // blue
-  // ];
-
   name = this.authStore.state.user?.firstName;
-  // photoUrl = this.authStore.state.user?.avatars;
 
   constructor(private authService: AuthService, private authStore: AuthStore) { }
 
@@ -34,14 +22,6 @@ export class MainLayoutComponent implements OnInit {
   }
 
   ngOnInit() {
-
-    // if (!this.photoUrl) {
-    //   this.showInitials = true;
-    //   this.createInititals();
-    //
-    //   const randomIndex = Math.floor(Math.random() * Math.floor(this.colors.length));
-    //   this.circleColor = this.colors[randomIndex];
-    // }
 
     this.items = [
       {
@@ -57,25 +37,5 @@ export class MainLayoutComponent implements OnInit {
       }
     ]
   }
-
-  // private createInititals(): void {
-  //   let initials = '';
-  //
-  //   for (let i = 0; i < this.name?.length; i++) {
-  //     if (this.name?.charAt(i) === '') {
-  //       continue;
-  //     }
-  //
-  //     if (this.name?.charAt(i) === this.name?.charAt(i).toUpperCase()) {
-  //       initials += this.name?.charAt(i);
-  //
-  //       if (initials.length === 2) {
-  //         break;
-  //       }
-  //     }
-  //   }
-  //
-  //   this.initials = initials;
-  // }
 
 }
