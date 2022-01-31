e260acf0c8d3b252183aba8cdf5e6a160fcde812
adi-matraku <matrakuadi@gmail.com>
2022-01-31 10:15:01 +0100

    Commit

--- src/app/pages/books/books.module.ts
+++ src/app/pages/books/books.module.ts
@@ -6,18 +6,25 @@ import { NewBookComponent } from './containers/new-book/new-book.component';
 import { ViewBookComponent } from './containers/view-book/view-book.component';
 import { EditBookComponent } from './containers/edit-book/edit-book.component';
 import { BooksFormComponent } from './components/books-form/books-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {NewLoanComponent} from "../loan/containers/new-loan/new-loan.component";
 
 const routes: Routes = [
   {
     path: '',
     component: BooksComponent
-  }
+  },
+  {
+    path: 'new',
+    component: NewBookComponent,
+  },
 ]
 
 @NgModule({
   imports:[
     RouterModule.forChild(routes),
     InputTextModule,
+    ReactiveFormsModule
   ],
   declarations: [
     BooksComponent,
--- src/app/pages/books/components/books-form/books-form.component.ts
+++ src/app/pages/books/components/books-form/books-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-books-form',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  form = this.fb.group({
+    isbn: ['test']
+  })
+
+  onSearch() {
+    console.log(this.form.value)
   }
 
 }
