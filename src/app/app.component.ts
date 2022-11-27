2e137b4b727c758fc2cce9883146ab005b27bb3d
adi-matraku <matrakuadi@gmail.com>
2022-11-27 19:52:45 +0100

    small fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthStore} from "./core/services/auth.store";
-import {take} from "rxjs";
-import {User} from "./models/user.model";
-import {AuthService} from "./pages/auth/services/auth.service";
+import { Component, OnInit } from '@angular/core';
+import { AuthStore } from './core/services/auth.store';
+import { take } from 'rxjs';
+import { User } from './models/user.model';
+import { AuthService } from './pages/auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <p-confirmDialog [breakpoints]="{'960px': '75vw', '640px': '100vw'}" [style]="{width: '35vw'}">
+  template: ` <p-confirmDialog
+      [breakpoints]="{ '960px': '75vw', '640px': '100vw' }"
+      [style]="{ width: '35vw' }"
+    >
       Content
     </p-confirmDialog>
     <p-toast position="top-right" key="toast"></p-toast>
@@ -16,29 +18,24 @@ import {AuthService} from "./pages/auth/services/auth.service";
 export class AppComponent implements OnInit {
   title = 'library-app';
 
-  constructor(
-    private authStore: AuthStore,
-    private authService: AuthService
-  ) {
-  }
+  constructor(private authStore: AuthStore, private authService: AuthService) {}
 
   ngOnInit() {
-    // const token = localStorage.getItem('token')
-    // if(token) {
-    //   this.authStore.setToken(token);
-    //   this.authService.me().pipe(take(1)).subscribe({
-    //     next: (me: User) => {
-    //       this.authStore.setUser(me)
-    //       console.log(this.authStore.state);
-    //     },
-    //     error: err => {
-    //       console.log(err);
-    //       this.authStore.setToken(null);
-    //       localStorage.removeItem('token');
-    //       //  shto nje toast qe shfaq err.message
-    //     }
-    //   })
-    // }
-
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authStore.setToken(token);
+      this.authService
+        .me()
+        .pipe(take(1))
+        .subscribe({
+          next: (me: User) => {
+            this.authStore.setUser(me);
+          },
+          error: (err) => {
+            this.authStore.setToken(null);
+            localStorage.removeItem('token');
+          },
+        });
+    }
   }
 }
